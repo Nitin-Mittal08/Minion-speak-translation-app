ab02e0aa12987a84b23bf8d4996d72ed7f713084
Nitin Mittal <nitin2000mittal@gmail.com>
2022-11-25 00:33:52 +0530

    Input and console

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-var userName = prompt("What is your name?")
-alert("External JS is working too "+ userName);
+//var userName = prompt("What is your name?")
+//alert("External JS is working too "+ userName);
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!");
+    console.log(txtInput.value);
+});
+
+
