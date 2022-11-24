81fa6d902892d3b48aaaec8cf655855cdb3f8eeb
Nitin Mittal <nitin2000mittal@gmail.com>
2022-11-25 00:51:56 +0530

    output sorted

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv  = document.querySelector("#output")
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!");
-    console.log(txtInput.value);
+    outputDiv.innerText = "translated " + txtInput.value;
 });
 
 
