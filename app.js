420f56c28664495266f618a81df9a49cf23430e5
Nitin Mittal <nitin2000mittal@gmail.com>
2022-11-25 15:16:11 +0530

    final changes for style

--- app.js
+++ app.js
@@ -4,8 +4,22 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv  = document.querySelector("#output")
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+function getTranslationURL(text){
+    return serverURL+"?text="+text;
+}
+
+function errorHandler(error){
+    console.log("error occured", error);
+}
 btnTranslate.addEventListener("click", function clickEventHandler(){
-    outputDiv.innerText = "translated " + txtInput.value;
+    var inputText = txtInput.value
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(input => outputDiv.innerText= input.contents.translated)
+    .catch(errorHandler);
+    
 });
 
 
